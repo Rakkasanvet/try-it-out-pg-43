f27d1ed25da8d447b29cc22d78f8cf524f52951c
Rakkasanvet <black6romeo@yahoo.com>
2015-11-24 21:11:51 -0600

    Initial commit

---
+++ try it out pg 43/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_it_out_pg_43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 5;
+            int h = 4;
+            double area = .5 * b * h;
+            
+
+        }
+    }
+}
